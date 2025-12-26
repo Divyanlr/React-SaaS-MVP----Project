74038051f5ef3d4c5c4dc8579e8459026d2846b4
Divya <divyadamodarannlr@gmail.com>
2025-12-26 17:37:36 +0100

    chore: setup React TypeScript and Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Tailwind is working!
+      </h1>
+    </>
+  );
+}
+
+export default App;
